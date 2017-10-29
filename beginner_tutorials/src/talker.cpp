434c98b97a15fd2a00e2a631a2ef30e2c14fcceb
rishabh1b <biyanirishabh@gmail.com>
2017-10-29 16:10:03 -0700

    Adding cpp-lint and cpp-check outputs

--- beginner_tutorials/src/talker.cpp
+++ beginner_tutorials/src/talker.cpp
@@ -4,10 +4,9 @@
  * @copyright BSD License (c) Rishabh Biyani
  */
 
+#include <sstream>
 #include "ros/ros.h"
 #include "std_msgs/String.h"
-
-#include <sstream>
 
 /**
  * This tutorial demonstrates simple sending of messages over the ROS system.
